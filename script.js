1234eab21628e8d2df8bfc7e6085059966a1a747
Manish Sharma <138285026+sharmamp07-17@users.noreply.github.com>
2024-06-19 21:45:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const navBar = document.querySelector('nav');
-const menuIcon = document.getElementById('menu')
-const navLinks = document.getElementById('tabs')
-const linkTabs = document.querySelectorAll('.tab-links');
-const container = document.querySelector('.container');
-// const closeIcon = document.querySelector('.fa-xmark')
+
 window.addEventListener('scroll', () => {
   if (window.scrollY >= 100) {
     navBar.classList.add('navBg')
@@ -13,14 +9,32 @@ window.addEventListener('scroll', () => {
   }
 });
 
+const menuIcon = document.getElementById('menu')
+const navLinks = document.getElementById('tabs')
+
 menuIcon.addEventListener('click', () => {
   navLinks.classList.toggle('navdisplay');
   menuIcon.classList.toggle('fa-xmark');
 });
 
+const linkTabs = document.querySelectorAll('.tab-links');
+
 linkTabs.forEach(tabs => {
   tabs.addEventListener('click', () => {
     navLinks.classList.remove('navdisplay')
     menuIcon.classList.remove('fa-xmark');
+
+    const current = document.getElementsByClassName('active');
+    current[0].className = current[0].className.replace(" active", "");
+    tabs.className += " active";
   })
-})
+});
+
+// HOME BUTTON FUNCTIONS
+
+const homeButton = document.getElementById('homebtn');
+
+homeButton.addEventListener('click', () => {
+  alert("It's Demo Page not ready for fully Fuctionally");
+  alert('Thanks for come to the Website 🙏')
+});
